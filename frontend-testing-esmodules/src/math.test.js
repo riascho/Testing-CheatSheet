ef20f1b1966112bfadc2686d09e6bc1888d64dcc
Ria <123465523+riascho@users.noreply.github.com>
2024-11-14 11:58:22 +0530

    adds more tests and fixes math unit

--- frontend-testing-esmodules/src/math.test.js
+++ frontend-testing-esmodules/src/math.test.js
@@ -12,3 +12,21 @@ it("should summarize all numbers in a given array", () => {
   // Assert
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const invalidNumbers1 = [1, 2, 3, "four"];
+  const invalidNumbers2 = ["one", 1, 2, 3];
+  const result1 = add(invalidNumbers1);
+  expect(result1).toBeNaN();
+  const result2 = add(invalidNumbers2);
+  expect(result2).toBeNaN();
+});
+
+it("should yield correct sum if input is numeric string values", () => {
+  const stringNumbers = ["1", "2", "3"];
+  const expectedResult = stringNumbers.reduce((acc, curr) => {
+    return Number(acc) + Number(curr);
+  });
+  const result = add(stringNumbers);
+  expect(result).toBe(expectedResult);
+});
