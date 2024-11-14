cd205dfe9168f994ac3a7ede108218868896b56a
Ria <123465523+riascho@users.noreply.github.com>
2024-11-14 18:47:48 +0530

    adds full covered test suites

--- frontend-testing-esmodules/src/math.test.js
+++ frontend-testing-esmodules/src/math.test.js
@@ -1,59 +1,61 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { add } from "./math";
 
-it("should summarize all numbers from array input", () => {
-  // Arrange
-  const numbers = [1, 2, 3, 4, 5];
-  const expectedResult = numbers.reduce((prev, curr) => {
-    return prev + curr;
+describe("add()", () => {
+  it("should summarize all numbers from array input", () => {
+    // Arrange
+    const numbers = [1, 2, 3, 4, 5];
+    const expectedResult = numbers.reduce((prev, curr) => {
+      return prev + curr;
+    });
+    // Act
+    const result = add(numbers);
+    // Assert
+    expect(result).toBe(expectedResult);
   });
-  // Act
-  const result = add(numbers);
-  // Assert
-  expect(result).toBe(expectedResult);
-});
 
-it("should yield NaN if input contains at least one invalid number", () => {
-  const invalidNumbers1 = [1, 2, 3, "four"];
-  const invalidNumbers2 = ["one", 1, 2, 3];
-  const result1 = add(invalidNumbers1);
-  expect(result1).toBeNaN();
-  const result2 = add(invalidNumbers2);
-  expect(result2).toBeNaN();
-});
-
-it("should yield correct sum if input is numeric string values", () => {
-  const stringNumbers = ["1", "2", "3"];
-  const expectedResult = stringNumbers.reduce((acc, curr) => {
-    return Number(acc) + Number(curr);
+  it("should yield NaN if input contains at least one invalid number", () => {
+    const invalidNumbers1 = [1, 2, 3, "four"];
+    const invalidNumbers2 = ["one", 1, 2, 3];
+    const result1 = add(invalidNumbers1);
+    expect(result1).toBeNaN();
+    const result2 = add(invalidNumbers2);
+    expect(result2).toBeNaN();
   });
-  const result = add(stringNumbers);
-  expect(result).toBe(expectedResult);
-});
 
-it("should yield 0 if input is empty array", () => {
-  const emptyInputArray = [];
-  const result = add(emptyInputArray);
-  expect(result).toBe(0);
-});
+  it("should yield correct sum if input is numeric string values", () => {
+    const stringNumbers = ["1", "2", "3"];
+    const expectedResult = stringNumbers.reduce((acc, curr) => {
+      return Number(acc) + Number(curr);
+    });
+    const result = add(stringNumbers);
+    expect(result).toBe(expectedResult);
+  });
 
-it("should throw error if no input provided", () => {
-  const resultFunction = () => add();
-  expect(resultFunction).toThrowError(TypeError);
-});
+  it("should yield 0 if input is empty array", () => {
+    const emptyInputArray = [];
+    const result = add(emptyInputArray);
+    expect(result).toBe(0);
+  });
 
-// alternative
-// it("should throw error if no input provided", () => {
-//   try {
-//     add();
-//   } catch (error) {
-//     expect(error).toBeInstanceOf(Error);
-//   }
-// });
+  it("should throw error if no input provided", () => {
+    const resultFunction = () => add();
+    expect(resultFunction).toThrowError(TypeError);
+  });
 
-it("should throw error if multiple arguments are passed instead of single array", () => {
-  const arg1 = 2;
-  const arg2 = 3;
-  const resultFunction = () => add(arg1, arg2);
-  expect(resultFunction).toThrowError(/is not iterable/);
+  // alternative
+  // it("should throw error if no input provided", () => {
+  //   try {
+  //     add();
+  //   } catch (error) {
+  //     expect(error).toBeInstanceOf(Error);
+  //   }
+  // });
+
+  it("should throw error if multiple arguments are passed instead of single array", () => {
+    const arg1 = 2;
+    const arg2 = 3;
+    const resultFunction = () => add(arg1, arg2);
+    expect(resultFunction).toThrowError(/is not iterable/);
+  });
 });
--- frontend-testing-esmodules/src/util/validation.test.js
+++ frontend-testing-esmodules/src/util/validation.test.js
@@ -1,16 +1,55 @@
-import { it, expect } from "vitest";
-import { validateStringNotEmpty, validateNumber } from "./validation";
+import { it, expect, describe } from "vitest";
 
-// happy path
-it("should throw error if input value is empty", () => {
-  const input = "";
-  const resultFunction = () => validateStringNotEmpty(input);
-  expect(resultFunction).toThrowError();
+import { validateStringNotEmpty } from "./validation";
+
+describe("validateStringNotEmpty()", () => {
+  // happy path
+  it("should throw 'must not be empty' error if input value is empty", () => {
+    const input = "";
+    const resultFunction = () => validateStringNotEmpty(input);
+    expect(resultFunction).toThrowError(/must not be empty/);
+  });
+
+  // unhappy path
+  it("should throw 'not a function' error if input is not string", () => {
+    const input = 345;
+    const resultFunction = () => validateStringNotEmpty(input);
+    expect(resultFunction).toThrowError(/is not a function/);
+  });
+
+  it("should throw 'cannot read properties of undefined' error if no input argument provided", () => {
+    const resultFunction = () => validateStringNotEmpty();
+    expect(resultFunction).toThrowError(/Cannot read properties of undefined/);
+  });
 });
 
-// happy path
-it("should throw error is input it not a number", () => {
-  const input = "two";
-  const resultFunction = () => validateNumber(input);
-  expect(resultFunction).toThrowError();
+import { validateNumber } from "./validation";
+
+describe("validateNumber()", () => {
+  // happy path
+  it("should throw 'Invalid number input' error if input is NaN or not of type number", () => {
+    const invalidString = "two";
+    const NaNInput = NaN;
+    const isInvalidString = () => validateNumber(invalidString);
+    const isAlreadyNaN = () => validateNumber(NaNInput);
+    const assertions = [isInvalidString, isAlreadyNaN];
+    assertions.forEach((func) => {
+      expect(func).toThrowError(/Invalid number input/);
+    });
+  });
+
+  // unhappy path
+  it("should also throw 'Invalid number input' error if no input argument provided", () => {
+    const resultFunction = () => validateNumber();
+    expect(resultFunction).toThrowError(/Invalid number input/);
+  });
+
+  it("should return undefined if input is a valid number or numeric string", () => {
+    const validNumber = 6;
+    const numericString = "6";
+    const result1 = validateNumber(validNumber);
+    const result2 = validateNumber(numericString);
+    expect(result1).toBeUndefined;
+    expect(result2).toBeUndefined;
+  });
 });
